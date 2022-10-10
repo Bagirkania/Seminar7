7cb2afeb905e5250f2f8a3076935a8fc23920af5
Bagirka <bruna07@mail.ru>
2022-10-10 20:47:57 +0500

    Task01

---
+++ Task/Task01/Program.cs
@@ -0,0 +1,39 @@
+﻿
+//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+void zadacha01()
+{
+Console.WriteLine("введите количество строк");
+int lines = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[lines, columns];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-10, 10));
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+            Console.Write(" \t ");
+        }
+        Console.WriteLine(" ");
+    }
+}
+}
+zadacha01();
+
+//Console.WriteLine($"значение элемента {n} строки и {m} столбца равно {numbers[n-1,m-1]}");
---
+++ Task/Task02/Program.cs
@@ -0,0 +1,50 @@
+﻿//Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+void zadach2()
+{
+Console.Clear();
+Console.WriteLine("введите номер строки");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите номер столбца");
+int m = Convert.ToInt32(Console.ReadLine());
+int [,] numbers = new int [10,10];
+FillArrayRandomNumbers(numbers);
+
+if ((m < numbers.GetLength(0) && m >= 0) || (n < numbers.GetLength(1) && m >= 0))
+{
+    Console.WriteLine($"Элемент в строке {m} столбце {n} равен : {numbers[m,n]}");
+}
+else Console.WriteLine("Такой позиции нет в массиве!");
+
+
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(1, 100);
+            }   
+        }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }   
+        Console.WriteLine(); 
+    }
+}
+}
+zadach2();
+
+
+
+
+
